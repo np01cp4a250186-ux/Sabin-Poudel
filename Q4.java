e9e61f744b349babe9a480781bb1f559138fe957
Sabin Poudel <np01cp4a250186@islingtoncollege.edu.np>
2025-12-16 15:07:50 +0545

    Initial sharing of project

---
+++ Q4.java
@@ -0,0 +1,20 @@
+
+/**
+ * Write a description of class Q4 here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class Q4
+{
+    public static void main(String[] args)
+    {
+       String namee= "sabin";
+       int age= 20;
+       float GPA= 3.47f;
+       System.out.println(namee);
+       System.out.println(age);
+       System.out.println(GPA);
+    }
+    
+}
---
+++ week2.java
@@ -0,0 +1,15 @@
+
+/**
+ * Write a description of class week2 here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class week2
+{
+    public static void main(String[] args)
+    {
+     String name = "sabin";
+     System.out.println("My name is: \n" + name);
+    }
+}
