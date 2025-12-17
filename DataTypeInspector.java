4961992cf72f72828afa2903b663a8181948b556
Sabin Poudel <np01cp4a250186@islingtoncollege.edu.np>
2025-12-17 16:08:26 +0545

    week3answer

---
+++ DataTypeInspector.java
@@ -0,0 +1,29 @@
+public class DataTypeInspector
+{
+    public static void main(String []args)
+    {
+        boolean myBoolean = true;
+        char myChar = 'A';
+        byte myByte = 120;
+        short myshort = 20000;
+        int myInt = 100000;
+        long mylong =500000;
+        float Myfloat = 10.05f;
+        double myDouble = 30.0010;
+        System.out.println("boolean: " + myBoolean);
+        System.out.println("char: " + myChar);
+        System.out.println("byte: " + myByte);
+        System.out.println("int: " + myInt);
+        System.out.println("long: " + mylong);
+        System.out.println("float: " + Myfloat);
+        System.out.println("short: " + myshort);
+        System.out.println("double: " + myDouble);
+        
+        
+        
+        
+        
+        
+        
+    }
+}
