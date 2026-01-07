502f42ab00fb066db68b44375552fa18f34293b0
Sabin Poudel <np01cp4a250186@islingtoncollege.edu.np>
2026-01-07 14:02:55 +0545

    ALl Question Completed

---
+++ Week8/ElectricityBillTest.java
@@ -0,0 +1,13 @@
+
+package Week8;
+public class ElectricityBillTest
+{
+    public static void main(String[] args)
+    {
+         ElectricityBill bill1 = new ElectricityBill("Ram Sharma", 150);
+         bill1.displayBill();
+         System.out.println();
+          bill1.setUnitsConsumed(90);
+        bill1.displayBill();
+    }
+}
